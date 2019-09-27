5f380011669801edd410adfd966ab3b81fa98eba
Fyzy <1164108627@qq.com>
2019-09-27 13:31:43 +0800

    init2

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 #3117008744 刘志豪
 #3117008747 谭万钏
 
-import re
+import re, os
+import check
+
+
+Formula = ['1', '+', '1' ]
+file = 'exercise.txt'
+flag = check.check(list, answer, file)
---
+++ check.py
@@ -0,0 +1,6 @@
+import os, re
+
+
+def check(list , e_file , a_file):
+    result = False
+    return result
